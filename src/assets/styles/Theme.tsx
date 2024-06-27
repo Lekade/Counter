4d6272882573a568fa37c8fa250c3ae23920e68d
Денис Касперович <kasperovich.deonis@gmail.com>
2024-06-27 20:25:00 +0300

    the global theme of the application

---
+++ src/assets/styles/Theme.tsx
@@ -0,0 +1,15 @@
+export type ThemeType = {
+    colors: {
+        primary: "#63DBFD",
+        bg: "#292C35",
+        error: "#EF1648"
+    }
+}
+export const Theme:ThemeType = {
+    colors : {
+        primary: "#63DBFD",
+        bg: "#292C35",
+        error: "#EF1648"
+
+    }
+}
