9aaa17ae481746b0e428ba1225cd01d2d29df046
Денис Касперович <kasperovich.deonis@gmail.com>
2024-08-04 14:51:52 +0300

    1) fewer props 2) come data from useSelect 3) changing data via dispatch 4) deleted all functions cheng data

--- src/components/counters/counter/CounterApp.tsx
+++ src/components/counters/counter/CounterApp.tsx
@@ -1,9 +1,11 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {Counter} from "./Counter";
 import Settings from "./Settings";
 import {Wrapper} from "./Counter.Styled";
+import {useSelector} from "react-redux";
+import {AppRootStateType} from "../store/Store";
 
-type DataType = {
+export type CounterStateType = {
     startValue : number
     maxValue: number
     data: number
@@ -12,63 +14,40 @@ type DataType = {
 }
 
 const CounterApp = () => {
-    const counterData = {
-        startValue : 0,
-        maxValue: 5,
-        data: 0,
-        enabledSettingsMod: false,
-        appError: false
-    }
-    const [counter, setCounter] = useState<DataType>(counterData)
 
-    useEffect(()=>{
-        const localStorageStartValue = localStorage.getItem('startValue')
-        const localStorageMaxValue = localStorage.getItem('maxValue')
-        if(localStorageStartValue && localStorageMaxValue){
-            const newStartValue = JSON.parse(localStorageStartValue)
-            const newMaxValue = JSON.parse(localStorageMaxValue)
-            setCounter(prevState => ({...prevState, startValue: newStartValue, maxValue: newMaxValue, data: newStartValue}))
-        }
-    }, [])
+    const counter = useSelector<AppRootStateType, CounterStateType>(state => state.counter)
+    const {startValue, maxValue, data, enabledSettingsMod, appError} = counter
 
-    const changeCounter = () => {
-        let newData = {...counter, data: ++counter.data}
-        setCounter(newData)
-    }
-    const resetCounter = () => {
-        let newData = {...counter, data: counter.startValue}
-        setCounter(newData)
-    }
-    const changeSettings = (maxValue: number, startValue: number) => {
-        setCounter( prevState => ({...prevState, maxValue, startValue, data: startValue }))
-        localStorage.setItem('startValue', JSON.stringify(startValue))
-        localStorage.setItem('maxValue', JSON.stringify(maxValue))
-    }
-    const onOffSettingsMod = (enabledSettingsMod: boolean) => {
-        setCounter(prevState =>({...prevState, enabledSettingsMod}))
-    }
-    const appErrorHandler = (appError: boolean) => {
-        setCounter(prevState =>({...prevState, appError}))
-    }
+
+    // useEffect(()=>{
+    //     const localStorageStartValue = localStorage.getItem('startValue')
+    //     const localStorageMaxValue = localStorage.getItem('maxValue')
+    //     if(localStorageStartValue && localStorageMaxValue){
+    //         const newStartValue = JSON.parse(localStorageStartValue)
+    //         const newMaxValue = JSON.parse(localStorageMaxValue)
+    //         setCounter(prevState => ({...prevState, startValue: newStartValue, maxValue: newMaxValue, data: newStartValue}))
+    //     }
+    // }, [])
+
+    // const changeSettings = (maxValue: number, startValue: number) => {
+    //     setCounter( prevState => ({...prevState, maxValue, startValue, data: startValue }))
+    //     localStorage.setItem('startValue', JSON.stringify(startValue))
+    //     localStorage.setItem('maxValue', JSON.stringify(maxValue))
+    // }
     return (
         <Wrapper>
             <Settings
-                startValue={counter.startValue}
-                maxValue={counter.maxValue}
-                changeSettings={changeSettings}
-                enabledSettingsMod={counter.enabledSettingsMod}
-                appError={counter.appError}
-                onOffSettingsMod={onOffSettingsMod}
-                appErrorHandler={appErrorHandler}
+                startValue={startValue}
+                maxValue={maxValue}
+                enabledSettingsMod={enabledSettingsMod}
+                appError={appError}
             />
             <Counter
-                counter={counter.data}
-                startValue={counter.startValue}
-                maxValue={counter.maxValue}
-                setCounter={changeCounter}
-                resetCounter={resetCounter}
-                enabledSettingsMod={counter.enabledSettingsMod}
-                appError={counter.appError}
+                counter={data}
+                startValue={startValue}
+                maxValue={maxValue}
+                enabledSettingsMod={enabledSettingsMod}
+                appError={appError}
             />
         </Wrapper>
     );
