20fc8f4599f5e51876cc403ae564332133613160
Денис Касперович <kasperovich.deonis@gmail.com>
2024-08-04 14:53:50 +0300

    1) fewer props 2) add functions cheng data via dispatch

--- src/components/counters/counter/Counter.tsx
+++ src/components/counters/counter/Counter.tsx
@@ -3,21 +3,19 @@ import '../counterTwo/Counter.css'
 import {Button} from "../../button/Button";
 import {BtnWrap, ColorType, CounterValue, Display, ErrorInfo, SettingsInfo, StyledCounter} from "./Counter.Styled";
 import {Theme} from "../../../assets/styles/Theme";
+import {useDispatch} from "react-redux";
+import {counterChangeAC, counterResetAC} from "../store/Counter-reducer";
 
 type CounterPropsType = {
     counter: number
     startValue: number
     maxValue: number
-    setCounter: () => void
-    resetCounter: () => void
     enabledSettingsMod: boolean
     appError: boolean
 }
 
 export const Counter = ({
                             counter,
-                            setCounter,
-                            resetCounter,
                             startValue,
                             maxValue,
                             enabledSettingsMod,
@@ -26,6 +24,16 @@ export const Counter = ({
     const color: ColorType = counter < maxValue ? Theme.colors.primary : Theme.colors.error
     const disabledInc = counter > maxValue - 1 || enabledSettingsMod
     const disabledReset = counter === startValue || enabledSettingsMod
+
+    const dispatch = useDispatch()
+
+    const counterChange = () => {
+        dispatch(counterChangeAC())
+    }
+    const counterReset = () => {
+        dispatch(counterResetAC())
+    }
+
 
     return (
         <StyledCounter>
@@ -44,8 +52,8 @@ export const Counter = ({
                 }
             </Display>
             <BtnWrap>
-                <Button disabled={disabledInc} onClickHandler={setCounter} styles={'btn'}>{'inc'}</Button>
-                <Button disabled={disabledReset} onClickHandler={resetCounter} styles={'btn'}>{'reset'}</Button>
+                <Button disabled={disabledInc} onClickHandler={counterChange} styles={'btn'}>{'inc'}</Button>
+                <Button disabled={disabledReset} onClickHandler={counterReset} styles={'btn'}>{'reset'}</Button>
             </BtnWrap>
         </StyledCounter>
     );
